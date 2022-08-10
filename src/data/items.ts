5602c4273fa7c195f710ce76cbd4388fe69e6959
Hudson Menezes <hudson.bm23@gmail.com>
2022-08-10 09:50:34 -0300

    Items and Items types added

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item'
+
+export const items:Item[] = [
+  { date: new Date(2022, 7, 10), category: 'food', title: 'McDonalds', value: 32.12 },
+  { date: new Date(2022, 7, 10), category: 'food', title: 'BurgerKing', value: 28 },
+  { date: new Date(2022, 7, 8), category: 'rent', title: 'Aluguel', value: 2300 },
+  { date: new Date(2022, 8, 10), category: 'salary', title: 'Frelance', value: 4500 },
+]
