1d35077a20f7036502fecfe9041b55d567259cfb
Hudson Menezes <hudson.bm23@gmail.com>
2022-08-15 11:58:26 -0300

    add info section created

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {items} from './data/items'
 import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter'
 import {TableArea} from './components/TableArea'
 import { InfoArea } from './components/InfoArea'
+import { InputArea } from './components/InputArea'
 
 const App = () => {
   const [list, setList] = useState(items)
@@ -41,6 +42,12 @@ const App = () => {
     setCurrentMonth(newMonth)
   }
 
+  const handleAddItem = (item: Item) =>{
+    let newList = [...list]
+    newList.push(item)
+    setList(newList)
+  }
+
   return (
     <C.Container>
       <C.Header>
@@ -53,7 +60,8 @@ const App = () => {
       onMonthChange={handleMonthChange} 
       income={income}
       expense={expense}/>
-      {/* Área de Inserção */}
+
+      <InputArea onAdd={handleAddItem} />
 
       {/* Área de Conteudo */}
       <TableArea list={filteredList}/>
